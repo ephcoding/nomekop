780eb53746989dc99c84ec884538fa79e82615fd
Eph <ephraim@modevx.com>
2023-08-17 17:37:54 -0500

    add service func to get single Pokemon

--- services/pokeapi.ts
+++ services/pokeapi.ts
@@ -6,13 +6,19 @@ const pokemonClient = axios.create({baseURL: 'https://pokeapi.co/api/v2'})
 
 export const getAllPokemon = async () => {
   try {
-    const response = await pokemonClient.get('/pokemon?limit=151&offset=0')
-    
-    if(response) console.log(response.data)
-    
+    const response = await pokemonClient.get('/pokemon?limit=151&offset=0')    
     return response.data.results
   } catch (error) {
     console.log("PokeAPI ERROR >>\n",error)
   }
 
+}
+
+export const getSinglePokemon = async (name: string) => {
+try {
+  const response = await pokemonClient.get(`/pokemon/${name}`)
+  return response.data
+} catch (error) {
+  console.log("PokeAPI ERROR >>\n",error)
+}
 }

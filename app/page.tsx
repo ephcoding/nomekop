3ab42067bc6d115150910f85bc67cc7c821210e8
Eph <ephraim@modevx.com>
2023-08-17 13:45:14 -0500

    remove styled template dev

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,13 @@
+import { SearchSection } from "@/components/SearchSection";
+
 export default function Home() {
 	return (
-		<div className='mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left'></div>
+		<>
+			{/* 
+				1. fetch all pokemon using react-query
+				2. render list of characters 
+			*/}
+			<SearchSection />
+		</>
 	);
 }

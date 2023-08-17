10082e156f0dd48740a6fa611988c38313631124
Eph <ephraim@modevx.com>
2023-08-17 16:15:04 -0500

    add results prop to getAllPokemon service func

--- services/pokeapi.ts
+++ services/pokeapi.ts
@@ -2,11 +2,15 @@ import axios from 'axios'
 
 const pokemonClient = axios.create({baseURL: 'https://pokeapi.co/api/v2'})
 
+
+
 export const getAllPokemon = async () => {
   try {
     const response = await pokemonClient.get('/pokemon?limit=151&offset=0')
     
-    return response.data
+    if(response) console.log(response.data)
+    
+    return response.data.results
   } catch (error) {
     console.log("PokeAPI ERROR >>\n",error)
   }

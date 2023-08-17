64020967a2e304059ec5f886d46a04572421d2b2
Eph <ephraim@modevx.com>
2023-08-17 13:33:36 -0500

    remove unused imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,3 @@
-import Image from "next/image";
-
-import { Card } from "@/components/Card";
-
 export default function Home() {
 	return (
 		<div className='mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left'></div>

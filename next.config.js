8d4b9e9ff6c477097afa315768c6b7149463e798
Eph <ephraim@modevx.com>
2023-08-17 17:37:39 -0500

    add remotePatterns config for fetching remote images

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
 	transpilePackages: ["react-daisyui"],
 	reactStrictMode: true,
+	images: {
+		remotePatterns: [
+			{ protocol: "https", hostname: "raw.githubusercontent.com" },
+		],
+	},
 };
 
 module.exports = nextConfig;

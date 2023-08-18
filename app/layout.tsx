b366c6d03bad53e70680a4619fb1c8fb97d14538
Ephraim <ephraim@modevx.com>
2023-08-18 00:40:19 -0500

    update page name

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,27 +6,27 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-	title: "Nomekop",
-	description: "Explore Your Favorite Pokemon",
+  title: "PokExplore",
+  description: "Explore Your Favorite Pokemon",
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: {
-	children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-	return (
-		<html lang='en'>
-			<body className={inter.className}>
-				<main className='flex min-h-screen flex-col items-center p-24'>
-					<div className='z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex'>
-						<NextLink href='/'>
-							<h2>NOMEKOP: Find Your Pokemon</h2>
-						</NextLink>
-					</div>
-					{children}
-				</main>
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <main className="flex min-h-screen flex-col items-center p-24">
+          <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+            <NextLink href="/">
+              <h2>PokExplore</h2>
+            </NextLink>
+          </div>
+          {children}
+        </main>
+      </body>
+    </html>
+  );
 }

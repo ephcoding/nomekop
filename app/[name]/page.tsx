06c7f5a14a3b3f65ed8ac16e1ab8ddd286622139
Ephraim <ephraim@modevx.com>
2023-08-18 00:51:46 -0500

    add dynamic stat bar colors

--- app/[name]/page.tsx
+++ app/[name]/page.tsx
@@ -18,6 +18,15 @@ export default async function PokemonPage({ params }: PageParams) {
   // const imageUrl = pokemon.sprites.other["official-artwork"].front_default;
   // const imageUrl = pokemon.sprites.other["official-artwork"].front_shiny;
 
+  const colorMap = {
+    hp: "primary",
+    attack: "secondary",
+    defense: "warning",
+    "special-attack": "info",
+    "special-defense": "error",
+    speed: "success",
+  };
+
   return (
     <>
       <h1 className="text-4xl text-bold pt-4">{name}</h1>
@@ -38,7 +47,7 @@ export default async function PokemonPage({ params }: PageParams) {
               <h3 className="p-3 w-2/4">
                 {statName}: {statValue}
               </h3>
-              <ProgressBar value={statValue} />
+              <ProgressBar value={statValue} color={colorMap[statName]} />
             </div>
           );
         })}
--- components/ProgressBar.tsx
+++ components/ProgressBar.tsx
@@ -1,6 +1,18 @@
 "use client";
 import { Progress } from "react-daisyui";
 
-export const ProgressBar = ({ value }: { value: number }) => {
-	return <Progress className='w-2/4 m-auto' value={value / 100} />;
+export const ProgressBar = ({
+  value,
+  color,
+}: {
+  value: number;
+  color: string;
+}) => {
+  return (
+    <Progress
+      className="w-2/4 m-auto bg-white"
+      color={color}
+      value={value / 100}
+    />
+  );
 };

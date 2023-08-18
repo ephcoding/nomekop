f6c4b7c28a1a9a7d5ed63d9fe41468ec044d781d
Ephraim <ephraim@modevx.com>
2023-08-17 23:27:24 -0500

    add sprite urls to see different pokemon pics

--- app/[name]/page.tsx
+++ app/[name]/page.tsx
@@ -5,40 +5,44 @@ import { getSinglePokemon } from "services/pokeapi";
 import { RemoteImage } from "@/components/RemoteImage";
 
 type PageParams = {
-	params: { name: string };
+  params: { name: string };
 };
 
 export default async function PokemonPage({ params }: PageParams) {
-	const { name } = params;
+  const { name } = params;
 
-	const pokemon = await getSinglePokemon(name);
-	const imageUrl = pokemon.sprites.other["official-artwork"].front_default;
+  const pokemon = await getSinglePokemon(name);
+  const imageUrl = pokemon.sprites.other.dream_world.front_default;
+  // const imageUrl = pokemon.sprites.other.home.front_default;
+  // const imageUrl = pokemon.sprites.other.home.front_shiny;
+  // const imageUrl = pokemon.sprites.other["official-artwork"].front_default;
+  // const imageUrl = pokemon.sprites.other["official-artwork"].front_shiny;
 
-	return (
-		<>
-			<h1 className='text-4xl text-bold pt-4'>{name}</h1>
-			<RemoteImage alt={name} imageUrl={imageUrl} />
+  return (
+    <>
+      <h1 className="text-4xl text-bold pt-4">{name}</h1>
+      <RemoteImage alt={name} imageUrl={imageUrl} />
 
-			<h3>Weight: {pokemon.weight}</h3>
-			<div className='flex-col'>
-				{pokemon.stats.map((statObject: any) => {
-					const statName = statObject.stat.name;
-					const statValue = statObject.base_stat;
+      <h3>Weight: {pokemon.weight}</h3>
+      <div className="flex-col">
+        {pokemon.stats.map((statObject: any) => {
+          const statName = statObject.stat.name;
+          const statValue = statObject.base_stat;
 
-					return (
-						<div
-							className='flex items-stretch'
-							style={{ width: "500px" }}
-							key={statName}
-						>
-							<h3 className='p-3 w-2/4'>
-								{statName}: {statValue}
-							</h3>
-							<ProgressBar value={statValue} />
-						</div>
-					);
-				})}
-			</div>
-		</>
-	);
+          return (
+            <div
+              className="flex items-stretch"
+              style={{ width: "500px" }}
+              key={statName}
+            >
+              <h3 className="p-3 w-2/4">
+                {statName}: {statValue}
+              </h3>
+              <ProgressBar value={statValue} />
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
 }

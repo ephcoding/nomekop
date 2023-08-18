3ea7bd6248d5e3c370a6b3fbc48d342f79a81c0a
Ephraim <ephraim@modevx.com>
2023-08-18 16:20:13 -0500

    create footer Link comopnent & add footer link hrefs

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,29 +3,35 @@ import { Footer as DaisyFooter } from "react-daisyui";
 import NextImage from "next/image";
 
 export const Footer = () => {
-	return (
-		<DaisyFooter className='bg-blue-500 p-5'>
-			<div style={{ position: "relative", width: "100px", height: "50px" }}>
-				<NextImage
-					alt='PokExplore logo'
-					src='/pokexplore-500x205-white.png'
-					fill
-					content='contain'
-				/>
-			</div>
-			<div>
-				<DaisyFooter.Title>PokÉxplore App</DaisyFooter.Title>
-				<a className='link link-hover'>Feedback</a>
-				<a className='link link-hover'>GitHub Repo</a>
-			</div>
-			<div>
-				<DaisyFooter.Title>Ephraim Smith</DaisyFooter.Title>
-				<a className='link link-hover'>ephraimsmith.dev</a>
-				<a className='link link-hover'>twitter</a>
-				<a className='link link-hover'>github</a>
-				<a className='link link-hover'>linkedin</a>
-				<a className='link link-hover'>instagram</a>
-			</div>
-		</DaisyFooter>
-	);
+  return (
+    <DaisyFooter className="bg-blue-500 p-5">
+      <div style={{ position: "relative", width: "100px", height: "50px" }}>
+        <NextImage
+          alt="PokExplore logo"
+          src="/pokexplore-500x205-white.png"
+          fill
+          content="contain"
+        />
+      </div>
+      <div>
+        <DaisyFooter.Title>PokÉxplore App</DaisyFooter.Title>
+        <Link href="mailto:ephraimjsmith@gmail.com">Feedback</Link>
+        <Link href="https://github.com/ephcoding/pokexplore">GitHub Repo</Link>
+      </div>
+      <div>
+        <DaisyFooter.Title>Ephraim Smith</DaisyFooter.Title>
+        <Link href="https://ephraimsmith.dev/projects">ephraimsmith.dev</Link>
+        <Link href="https://twitter.com/ephraimsmithdev">twitter</Link>
+        <Link href="https://github.com/ephcoding">GitHub</Link>
+        <Link href="https://linkedin.com/in/ephraimsmithdev">LinkedIn</Link>
+        <Link href="https://instagram.com/ephraimsmithdev">Instagram</Link>
+      </div>
+    </DaisyFooter>
+  );
 };
+
+const Link = ({ children, href }) => (
+  <a target="_blank" className="link link-hover" href={href}>
+    {children}
+  </a>
+);
